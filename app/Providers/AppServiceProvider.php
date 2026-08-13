42433d1d58299b6c4ea0c19d3513360e8032d2d1
salmudiyanto <30875525+salmudiyanto@users.noreply.github.com>
2026-08-13 22:29:07 +0800

    Add ODBC driver support in AppServiceProvider and test DB connection to SQL Anywhere 11 (simpada)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \DB::extend('odbc', function($config, $name) {
+            $dsn = isset($config['dsn']) && !empty($config['dsn']) ? $config['dsn'] : $config['database'];
+            if (strpos($dsn, 'odbc:') !== 0) {
+                $dsn = 'odbc:' . $dsn;
+            }
+            $user = isset($config['username']) ? $config['username'] : '';
+            $pass = isset($config['password']) ? $config['password'] : '';
+            $options = isset($config['options']) ? $config['options'] : [];
+
+            $pdo = new \PDO($dsn, $user, $pass, $options);
+            return new \Illuminate\Database\Connection($pdo, $config['database'], isset($config['prefix']) ? $config['prefix'] : '', $config);
+        });
     }
 
     /**
